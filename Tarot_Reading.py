98fadbced3600f6f02942bda610ac06a21085574
CosmicZep <107496135+CosmicZep@users.noreply.github.com>
2022-08-25 08:50:14 -0500

    Update and rename Tarot Reading to Tarot_Reading.py

--- Tarot_Reading.py
+++ Tarot_Reading.py
@@ -1,3 +1,17 @@
+# Pip file
+[[source]]
+url = "https://pypi.org/simple"
+verify_ssl = true
+name = "pypi"
+
+[packages]
+
+[dev-packages]
+
+[requires]
+python_version = "3.10"
+
+# Function Code
 tarot = { 1:	"The Magician", 2:	"The High Priestess", 3:	"The Empress", 4:	"The Emperor", 5:	"The Hierophant", 6:	"The Lovers", 7:	"The Chariot", 8:	"Strength", 9:	"The Hermit", 10:	"Wheel of Fortune", 11:	"Justice", 12:	"The Hanged Man", 13:	"Death", 14:	"Temperance", 15:	"The Devil", 16:	"The Tower", 17:	"The Star", 18:	"The Moon", 19:	"The Sun", 20:	"Judgement", 21:	"The World", 22: "The Fool"}
 
 import random
